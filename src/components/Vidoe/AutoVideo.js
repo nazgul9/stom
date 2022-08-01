46b08f308204bdd8e5f0c65934f8e3fafae08323
nazgul9 <nazgul.abdibalieva.kk@gmail.com>
2022-08-01 16:34:14 +0600

    about

---
+++ src/components/Vidoe/AutoVideo.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import v from './v.mp4'
+
+// function AutoVideo(props) {
+//     return (
+//         <div className={"auto_video mt-3"}>
+//             <video className='vidoe' width="320" height="240" autoPlay controls>
+//                 <source src={"v.mp4"} />
+//             </video>
+
+//         </div>
+//     );
+// }
+
+// export default AutoVideo;
---
+++ src/components/accrdion/Accordion.js
@@ -0,0 +1,48 @@
+// import React from 'react';
+
+// function Accordion(props) {
+//     return (
+//         <div>
+//             <div class="accordion bg-light">
+
+// <div className='bg-light'>
+//   <input type="checkbox" name="panel" id="panel-1"/>
+//   <label for="panel-1 text-white">Panel 1</label>
+//   <div class="accordion__content accordion__content--small">
+//     <h2 class="accordion__header  text-white">Header</h2>
+//     <p class="accordion__body ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet.</p>
+//   </div>
+// </div>
+
+// <div>
+//   <input type="checkbox" name="panel" id="panel-2"/>
+//   <label for="panel-2">Panel 2</label>
+//   <div class="accordion__content accordion__content--med">
+//     <h2 class="accordion__header">Header</h2>
+//     <p class="accordion__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente eligendi nulla illo culpa ab in at adipisci eveniet id itaque maxime soluta recusandae doloribus laboriosam dignissimos est aut cupiditate delectus.</p>
+//   </div>
+// </div>
+// <div>
+//   <input type="checkbox" name="panel" id="panel-3"/>
+//   <label for="panel-3">Panel 3</label>
+//   <div class="accordion__content accordion__content--small">
+//     <h2 class="accordion__header">Header</h2>
+//     <p class="accordion__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+//   </div>
+// </div>
+
+// <div>
+//   <input type="checkbox" name="panel" id="panel-4"/>
+//   <label for="panel-4">Panel 4</label>
+//   <div class="accordion__content accordion__content--large">
+//     <h2 class="accordion__header">Header</h2>
+//     <p class="accordion__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda odio provident ullam culpa rem tempora voluptatem inventore facere adipisci doloribus dolorum ad maxime itaque quasi animi aliquid voluptates rerum expedita? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui culpa amet neque nostrum cumque eaque corrupti ad accusantium? Consectetur reiciendis ad earum aspernatur at quibusdam cupiditate rerum ipsam consequatur suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab rem explicabo exercitationem unde harum? Iste nobis quae animi illum laborum incidunt hic illo ad repellat repudiandae et alias facere magni.</p>
+//   </div>
+// </div>
+
+// </div>
+//         </div>
+//     );
+// }
+
+// export default Accordion;
